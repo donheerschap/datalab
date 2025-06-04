05a0695356ac57e3529de163b3994f29a8eac329
Don Heerschap <don.heerschap@inspark.nl>
2025-06-04 15:56:10 +0200

    fix: update max_size_gb and sku_name for WorldWideImporters database

--- infra/database.tf
+++ infra/database.tf
@@ -33,9 +33,9 @@ resource "azurerm_mssql_database" "wwi_database" {
   server_id      = azurerm_mssql_server.sql_server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 32
+  max_size_gb    = 2
   read_scale     = false
-  sku_name       = "S0"
+  sku_name       = "Basic"
   zone_redundant = false
 
   tags = {
